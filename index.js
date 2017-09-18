f5bcb58228f833a198071a3d320a275a5f0fa786
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-18 19:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function checkCollision(rock) {
       ((rockLeftEdge <= dodgerLeftEdge) && (rockRightEdge >= dodgerLeftEdge)) ||
       ((rockLeftEdge >= dodgerLeftEdge) && (rockRightEdge <= dodgerRightEdge)) ||
       ((rockLeftEdge <= dodgerRightEdge) && (rockRightEdge >= dodgerRightEdge)) ){
-        return false;
+        return true;
       }
       /**
                * Think about it -- what's happening here?
