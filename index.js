9afb52c82c17de05ad13446a105582824cc67217
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-18 18:40:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -156,7 +156,8 @@ function moveDodger(e) {
    //$(document).on('keyleft', function(e){
 
    document.addEventListener('keydown', function(e){
-     if(e.which === RIGHT_ARROW){
+     if(e.which === RIGHT_ARROW && e.stopPropagation){
+       e.stopPropagation();
        moveDodgerRight();
      }
      if(e.which === LEFT_ARROW){
