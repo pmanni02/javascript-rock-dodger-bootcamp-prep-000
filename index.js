d89e9ab47e10397857247ca1d8fc1bf7b54963ad
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-18 17:40:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -191,11 +191,11 @@ function moveDodgerRight() {
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
    var dodgerRightEdge = positionToInteger(DODGER.style.left) + 40;
-   
+
       //MOVE right changing dodger.style.left
    function step(){
-     DODGER.style.left = `${DODGER.style.left += 4}px`;
-     //DODGER.style.left = `${dodgerRightEdge + 4}px`;
+     //DODGER.style.left = `${DODGER.style.left += 4}px`;
+     DODGER.style.left = `${dodgerRightEdge + 4}px`;
      if(dodgerRightEdge <= 360){
        window.requestAnimationFrame(step);
      }
